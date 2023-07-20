9ed8af87c3cd290db463299fc94827bb001b5129
arunma <arunma@tencent.com>
2023-07-20 16:53:14 +0800

    fix: modify modules

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+module "cvm_image" {
+  source        = "./modules/image"
+  image_version = var.image_version
+}
+
+module "vpc" {
+  source            = "./modules/vpc"
+  availability_zone = var.availability_zone
+}
+
+module "clb" {
+  source = "./modules/clb"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "as_scaling" {
+  source          = "./modules/as"
+  clb_internal_id = module.clb.clb_instance_id
+  clb_listener_id = module.clb.clb_listener_id
+  image_id        = module.cvm_image.image_id
+  vpc_id          = module.vpc.vpc_id
+  subnet_id       = module.vpc.subnet_id
+}
+
+
+module "vpc2" {
+  source = "./modules/vpc"
+  availability_zone = ""
+}
---
+++ terraform/modules/clb/outputs.tf
@@ -0,0 +1,9 @@
+output "clb_instance_id" {
+  value       = tencentcloud_clb_instance.clb_internal.id
+  description = "clb instance Id"
+}
+
+output "clb_listener_id" {
+  value       = tencentcloud_clb_listener.clb_listener.id
+  description = "clb listener Id"
+}
